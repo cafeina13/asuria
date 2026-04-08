86d92a45b3a767230a6a716719b26a1f69055d47
ahmoali32 <ulukoseahmetali032@gmail.com>
2026-04-08 03:44:58 +0300

    oyun için: kullanıcı girişi eklendi, kayıt olma eklenecek.

--- api/user-handler.js
+++ api/user-handler.js
@@ -1,38 +1,65 @@
 
-  const KAPALI_YAZISI = "Bu API şu anda Dans Etmeye gitti ne zaman gelir belli değil.";
+const KAPALI_YAZISI = "Bu API şu anda Dans Etmeye gitti ne zaman gelir belli değil.";
 
 export default async function handler(req, res) {
 
+  
+  const token = process.env.GITHUB_TOKEN;
+  const REPO_OWNER = "cafeina13";
+  const REPO_NAME = "asuria";
+  const jsonUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/users.json`;
+  const jsonLocalUrl = "http://localhost:3000/users.json" 
+  /*
   try {
-      const token = process.env.GITHUB_TOKEN;
-    
+
     if (token) {
       const jsonCheckRes = await fetch("https://api.github.com/repos/cafeina13/asuria/contents/apis.json", {
-        headers: { 
+        headers: {
           'Authorization': `token ${token}`,
-          'Accept': 'application/vnd.github.v3.raw' 
+          'Accept': 'application/vnd.github.v3.raw'
         }
       });
 
       if (jsonCheckRes.ok) {
         const apisList = await jsonCheckRes.json();
-        
+
         const buApi = apisList.find(api => api.slug === "user-handler");
-        
+
         if (buApi && buApi.isActive === false) {
           return response.status(200).send(KAPALI_YAZISI);
         }
       }
     }
   }
-    catch (error) {return res.status(500).send("Hata oluştu: " + error.message);}
+  catch (error) { return res.status(500).send("Hata oluştu: " + error.message); }
+*/
+  const username = req.query.username;
+  const password = req.query.password;
 
-  try {
-    const response = await fetch("katana");
-    const data = await response.text();
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    return res.status(200).send(data);
-  } catch (e) {
-    return res.status(500).send("Hata oluştu");
+
+  if (req.method === "GET") {
+    const getJson = await fetch(jsonLocalUrl, {
+      /*headers: { Authorization: `token ${token}` },*/
+    });
+
+    if (getJson.ok) {
+      const usersGet = await getJson.json();
+      /*const users = JSON.parse(
+        Buffer.from(data.content, "base64").toString("utf-8"),
+      );*/
+      //const users = JSON.parse(data);
+      const users = usersGet.users;
+      let user;
+      if (users[username].trustMeBroPassword === password)
+        user = users[username];
+      else
+        return res.status(401).json({ hata: "Yetkisiz erişim! İsim ve Şifre Eşleşmedi!" });
+      return res.status(200).json(user);
+    }
+    return res.status(500).json([]);
+  }
+
+  if (req.method === "POST") {
+
   }
 }
--- scripts/game.js
+++ scripts/game.js
@@ -4,30 +4,30 @@ let manuelButGucu = 1;
 let signedIn = false;
 
 let akademisyenList = [
-    { name: "Fatih Hoca", cost: 10, effect: "Otomatik Büt", effectAciklama: "Her saniye 1 büt üretir", aktif: false, buff: ["adder", 1] },
-    { name: "Lain Hoca", cost: 50, effect: "Otomatik 5 Büt", effectAciklama: "Her saniye 5 büt üretir", aktif: false, buff: ["adder", 5] },
-    { name: "Fatma Hoca", cost: 300, effect: "Otomatik 20 Büt", effectAciklama: "Her saniye 20 büt üretir", aktif: false, buff: ["adder", 20] },
-    { name: "Sena Hoca", cost: 2000, effect: "Double Büt çarpanı (2x)", effectAciklama: "Büt çarpanını iki katına çıkarır, en son hesaplanır", aktif: false, buff: ["special", 2] },
-    { name: "Samet Hoca", cost: 3000, effect: "OtoBüt çarpanı +1", effectAciklama: "Büt çarpanına 1 ekler", aktif: false, buff: ["multiplier", 1] },
-    { name: "Erdem Hoca", cost: 12000, effect: "OtoBüt çarpanı +2", effectAciklama: "Büt çarpanına 2 ekler", aktif: false, buff: ["multiplier", 2] },
-    { name: "Gizem Hoca", cost: 100000, effect: "OtoBüt çarpanı +5", effectAciklama: "Büt çarpanına 5 ekler", aktif: false, buff: ["multiplier", 5] },
-    { name: "Jhonny Test", cost: 300000, effect: "Quadra Büt Çarpanı (4x)", effectAciklama: "Büt çarpanını 4 katına çıkarır, en son hesaplanır", aktif: false, buff: ["special", 4] },
-    { name: "Mordecai Hoca", cost: 1000000, effect: "OtoBüt çarpanı +10", effectAciklama: "Büt çarpanına 10 ekler", aktif: false, buff: ["multiplier", 10] },
-    { name: "Rektör Skips", cost: 4000000, effect: "OtoBüt çarpanı +20", effectAciklama: "Büt çarpanına 20 ekler", aktif: false, buff: ["multiplier", 20] },
-    { name: "Rigby KNK", cost: 10000000, effect: "OtoBüt çarpanı +50", effectAciklama: "Büt çarpanına 50 ekler", aktif: false, buff: ["multiplier", 50] },
-    { name: "Attack Shark", cost: -10000000, effect: "OtoBüt çarpanı +150", effectAciklama: "Büt çarpanına 50 ekler", aktif: false, buff: ["multiplier", 150] },
+    { id: 0, name: "Fatih Hoca", cost: 10, effect: "Otomatik Büt", effectAciklama: "Her saniye 1 büt üretir", aktif: false, buff: ["adder", 1] },
+    { id: 1, name: "Lain Hoca", cost: 50, effect: "Otomatik 5 Büt", effectAciklama: "Her saniye 5 büt üretir", aktif: false, buff: ["adder", 5] },
+    { id: 2, name: "Fatma Hoca", cost: 300, effect: "Otomatik 20 Büt", effectAciklama: "Her saniye 20 büt üretir", aktif: false, buff: ["adder", 20] },
+    { id: 3, name: "Sena Hoca", cost: 2000, effect: "Double Büt çarpanı (2x)", effectAciklama: "Büt çarpanını iki katına çıkarır, en son hesaplanır", aktif: false, buff: ["special", 2] },
+    { id: 4, name: "Samet Hoca", cost: 3000, effect: "OtoBüt çarpanı +1", effectAciklama: "Büt çarpanına 1 ekler", aktif: false, buff: ["multiplier", 1] },
+    { id: 5, name: "Erdem Hoca", cost: 12000, effect: "OtoBüt çarpanı +2", effectAciklama: "Büt çarpanına 2 ekler", aktif: false, buff: ["multiplier", 2] },
+    { id: 6, name: "Gizem Hoca", cost: 100000, effect: "OtoBüt çarpanı +5", effectAciklama: "Büt çarpanına 5 ekler", aktif: false, buff: ["multiplier", 5] },
+    { id: 7, name: "Jhonny Test", cost: 300000, effect: "Quadra Büt Çarpanı (4x)", effectAciklama: "Büt çarpanını 4 katına çıkarır, en son hesaplanır", aktif: false, buff: ["special", 4] },
+    { id: 8, name: "Mordecai Hoca", cost: 1000000, effect: "OtoBüt çarpanı +10", effectAciklama: "Büt çarpanına 10 ekler", aktif: false, buff: ["multiplier", 10] },
+    { id: 9, name: "Rektör Skips", cost: 4000000, effect: "OtoBüt çarpanı +20", effectAciklama: "Büt çarpanına 20 ekler", aktif: false, buff: ["multiplier", 20] },
+    { id: 10, name: "Rigby KNK", cost: 10000000, effect: "OtoBüt çarpanı +50", effectAciklama: "Büt çarpanına 50 ekler", aktif: false, buff: ["multiplier", 50] },
+    { id: 11, name: "Attack Shark", cost: -10000000, effect: "OtoBüt çarpanı +150", effectAciklama: "Büt çarpanına 50 ekler", aktif: false, buff: ["multiplier", 150] },
 ];
 
 let kopyaList = [
-    { name: "Sıraya Not", cost: 100, effect: "Manuel Büt +1", effectAciklama: "Yazın çok kötü, her deneme +1 büt", aktif: false, buff: ["adder", 2] },
-    { name: "Kopya Kağıdı", cost: 500, effect: "manuel büt +10", effectAciklama: "Yazın hâlâ kötü, her deneme +10 büt", aktif: false, buff: ["adder", 50] },
-    { name: "Yanındakine Bakış", cost: 2000, effect: "Manuel büt +30", effectAciklama: "Yanındaki de bütünlemeye girdiğine göre, +30", aktif: false, buff: ["adder", 200] },
-    { name: "Önündekine bakış", cost: 10000, effect: "2 Kat Manuel Büt kazancı", effectAciklama: "Gözün bozuk, her denemede 2 kat büt (en son hesaplanır) ", aktif: false, buff: ["special", 2] },
-    { name: "Arkadaşınla Göz Teması", cost: 50000, effect: "Manuel Büt kazancı +20%", effectAciklama: "Arkadaşınla göz teması kurdun ama yanlış anladın, her denemede %20 daha fazla büt", aktif: false, buff: ["multiplier", 1.2] },
-    { name: "Hocaya Sor", cost: 200000, effect: "Manuel Büt kazancı +100", effectAciklama: "Hocaya soru sordun ama o da bütünlemeye girdiğine göre, her denemede +100 büt", aktif: false, buff: ["adder", 100] },
-    { name: "Kopya Çekme Sanatı", cost: 1000000, effect: "3 kat Manuel Büt kazancı çarpanı", effectAciklama: "Kopya çekme sanatında ustalaştın artık önümüzdeki bütlere bakıcaz, her denemede 3x büt", aktif: false, buff: ["multiplier", 3] },
-    { name: "Psişik Kopya", cost: 5000000, effect: "5 kat Manuel Büt kazancı çarpanı", effectAciklama: "Psişik güçlerinle kopya çekmeye başladın, her denemede 5x büt", aktif: false, buff: ["multiplier", 5] },
-    { name: "Sallama", cost: 10000000, effect: "-10 kat Manuel Büt kazancı çarpanı", effectAciklama: "Sallamaya başladın, artık bütlerin azlıyor, her denemede -10x büt", aktif: false, buff: ["special", -10] },
+    { id: 0, name: "Sıraya Not", cost: 100, effect: "Manuel Büt +1", effectAciklama: "Yazın çok kötü, her deneme +1 büt", aktif: false, buff: ["adder", 2] },
+    { id: 1, name: "Kopya Kağıdı", cost: 500, effect: "manuel büt +10", effectAciklama: "Yazın hâlâ kötü, her deneme +10 büt", aktif: false, buff: ["adder", 50] },
+    { id: 2, name: "Yanındakine Bakış", cost: 2000, effect: "Manuel büt +30", effectAciklama: "Yanındaki de bütünlemeye girdiğine göre, +30", aktif: false, buff: ["adder", 200] },
+    { id: 3, name: "Önündekine bakış", cost: 10000, effect: "2 Kat Manuel Büt kazancı", effectAciklama: "Gözün bozuk, her denemede 2 kat büt (en son hesaplanır) ", aktif: false, buff: ["special", 2] },
+    { id: 4, name: "Arkadaşınla Göz Teması", cost: 50000, effect: "Manuel Büt kazancı +20%", effectAciklama: "Arkadaşınla göz teması kurdun ama yanlış anladın, her denemede %20 daha fazla büt", aktif: false, buff: ["multiplier", 1.2] },
+    { id: 5, name: "Hocaya Sor", cost: 200000, effect: "Manuel Büt kazancı +100", effectAciklama: "Hocaya soru sordun ama o da bütünlemeye girdiğine göre, her denemede +100 büt", aktif: false, buff: ["adder", 100] },
+    { id: 6, name: "Kopya Çekme Sanatı", cost: 1000000, effect: "3 kat Manuel Büt kazancı çarpanı", effectAciklama: "Kopya çekme sanatında ustalaştın artık önümüzdeki bütlere bakıcaz, her denemede 3x büt", aktif: false, buff: ["multiplier", 3] },
+    { id: 7, name: "Psişik Kopya", cost: 5000000, effect: "5 kat Manuel Büt kazancı çarpanı", effectAciklama: "Psişik güçlerinle kopya çekmeye başladın, her denemede 5x büt", aktif: false, buff: ["multiplier", 5] },
+    { id: 8, name: "Sallama", cost: 10000000, effect: "-10 kat Manuel Büt kazancı çarpanı", effectAciklama: "Sallamaya başladın, artık bütlerin azlıyor, her denemede -10x büt", aktif: false, buff: ["special", -10] },
 ];
 function updateOtoBut(yenidenHesapla = false) {
     if (!yenidenHesapla) {
@@ -81,12 +81,16 @@ function updateManuelBut() {
 const leftHoverPanelCheck = document.getElementById('left-hover-check');
 const leftHoverPanel = document.getElementById('left-hover-panel')
 const leftHoverPanelContent = document.getElementById('left-hover-panel-content')
+const saveButton = document.getElementById('btn-signSave')
 const bg_dark = document.getElementById('bg-dark');
 
 
 leftHoverPanelCheck.addEventListener('mouseenter', () => {
     bg_dark.classList.add('show');
     
+    if (signedIn) {saveButton.style.display = 'flexible';} 
+    else {saveButton.style.display = 'none';}
+
     leftHoverPanel.style.display = 'flex';
     setTimeout(() => {
         leftHoverPanel.classList.remove('close');
@@ -111,6 +115,7 @@ function updateGUI() {
     document.getElementById('manuelbut').textContent = manuelButGucu
 }
 
+//Hover Pencere, Kayıt ol-----------------------------------------------
 const btnSignUp = document.getElementById('btn-signUp');
 const signUpModal = document.getElementById('sign-up-modal');
 const closeSignUpModal = document.getElementById('close-sign-up-Modal');
@@ -123,18 +128,72 @@ closeSignUpModal.addEventListener('click', () => {
     signUpModal.style.display = 'none';
 });
 
-const nameTextBox = document.getElementById('username-up-box');
-const passwordTextBox = document.getElementById('password-up-box');
-const kayitOlBTN = document.getElementById('btn-signIn-check');
+//Kayıt ol
+const nameUpTextBox = document.getElementById('username-up-box');
+const passwordUpTextBox = document.getElementById('password-up-box');
+const kayitOlBTN = document.getElementById('btn-signUp-check');
 
 kayitOlBTN.addEventListener('click', () => {
-    name = nameTextBox.textContent;
-    password = passwordTextBox.textContent;
+    let name = nameUpTextBox.textContent;
+    let password = passwordUpTextBox.textContent;
 
-    
+
 })
 
+// Hover Pencere, Giriş yap---------------------------------------------
+const btnSignIn = document.getElementById('btn-signIn');
+const signInModal = document.getElementById('sign-in-modal');
+const closeSignInModal = document.getElementById('close-sign-in-Modal');
 
+btnSignIn.addEventListener('click', () => {
+    signInModal.style.display = 'flex';
+});
+
+closeSignInModal.addEventListener('click', () => {
+    signInModal.style.display = 'none';
+});
+
+const nameInTextBox = document.getElementById('username-in-box');
+const passwordInTextBox = document.getElementById('password-in-box');
+const girisBTN = document.getElementById('btn-signIn-check');
+
+let nameIn = nameInTextBox.value;
+let passwordIn = passwordInTextBox.value;
+
+girisBTN.addEventListener('click', async () => {
+    nameIn = nameInTextBox.value != "" ? nameInTextBox.value : "sherlock" ;
+    passwordIn = passwordInTextBox.value;
+
+    const getJson = await fetch(`../api/user-handler.js?username=${nameIn}&password=${passwordIn}`);
+    const userHanger = await getJson.json();
+    if (getJson.ok){
+        document.getElementById('username-dyn').textContent = userHanger.name;
+        closeSignInModal.click();
+
+        butSayisi = userHanger.score;
+
+        userHanger.upgrades.up1.forEach((deger,index) => {
+            akademisyenList[id=index].aktif = deger;
+        });
+
+        userHanger.upgrades.up2.forEach((deger,index) => {
+            kopyaList[id=index].aktif = deger;
+        });
+
+
+        updateManuelBut();
+        updateOtoBut(true);
+        manuelButGucu = manuelButGucu === 0 ? 1 : manuelButGucu;
+        updateGUI();
+
+        showToast(`Kimlik değiştirildi, Yeni kullanıcı ${userHanger.name}`)
+
+    }
+    else if (getJson.status == 500) {alert("Json Kapalı")}
+    else if (getJson.status == 401) {alert(userHanger.hata)}
+});
+
+//Render Akademisyen satın alma ekranı --------------------------------
 function renderKadro() {
     const shopList = document.getElementById('kadro-list');
     shopList.innerHTML = "";
@@ -161,7 +220,7 @@ function renderKadro() {
     });
 }
 
-
+//Render kopya çeşidi satın alma ekranı ---------------------------------
 function renderKopyaSheet() {
     const shopList = document.getElementById('kopya-list');
     shopList.innerHTML = "";
@@ -188,6 +247,7 @@ function renderKopyaSheet() {
     });
 }
 
+// Akademisyen kesinleştirme satın alma sırasında çalışacak kodlar------
 function kesinlestir(index) {
     let hoca = akademisyenList[index];
 
@@ -204,6 +264,7 @@ function kesinlestir(index) {
     }
 }
 
+//Kopya satın alma sırasında çalışacak kodlar --------------------------
 function kopyaCek(index) {
     let kopya = kopyaList[index];
 
